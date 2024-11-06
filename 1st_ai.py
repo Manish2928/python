0106095ed993499d65cb963f891285bb3944be55
Manish2928 <manishkashyap9611@gmail.com>
2024-11-06 19:48:27 +0530

    ERROR fix

--- 1st_ai.py
+++ 1st_ai.py
@@ -1,5 +1,21 @@
 #Max from 3
 
+def max_from_three(a,b,c):
+    if a>b and a>c:
+        return "a is MAX "
+    elif b>a and b>c:
+        return "b is MAX "
+    elif c>a and c>b:  
+        return "c is MAX "
+    else:
+        return "All numbers are equal."
     
     
     
+a1 = int(input("a : "))
+a2 = int(input("b : "))
+a3 = int(input("c : "))
+
+ans = max_from_three(a1,a2,a3)
+
+print (ans)
